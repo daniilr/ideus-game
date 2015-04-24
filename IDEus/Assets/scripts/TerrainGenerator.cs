a4850af8c0b9ae7614d99178b6d9f4c983e9959e
Даниил Рыжков <daniil.re@gmail.com>
2015-04-24 23:12:20 +1000

    Getting project files back. Gitignore update

---
+++ IDEus/Assets/scripts/TerrainGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGenerator : MonoBehaviour {
+	public GameObject Block;
+	// Use this for initialization
+	void Start () {
+		GameObject clone;
+		for (int i = 0; i < 10; i++) {
+			clone = Instantiate (Block);
+			clone.transform.position = Vector3(0,0,0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
