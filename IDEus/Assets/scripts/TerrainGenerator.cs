ec64a6835c30aadbf6d71cf9c6ef7bf90f127aec
Даниил Рыжков <daniil.re@gmail.com>
2015-04-25 06:43:25 +1000

    Added friction to the blocks

--- IDEus/Assets/scripts/TerrainGenerator.cs
+++ IDEus/Assets/scripts/TerrainGenerator.cs
@@ -5,11 +5,13 @@ public class TerrainGenerator : MonoBehaviour {
 	public GameObject Block;
 	// Use this for initialization
 	void Start () {
+		int[] level = new int[5] {1, 1, 1, 2, 1};
 		GameObject clone;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < level.Length; i++) {
 			clone = Instantiate (Block);
 			clone.transform.Translate(new Vector3(i*1.5f, 0, 0));
 		}
+
 	}
 	
 	// Update is called once per frame
