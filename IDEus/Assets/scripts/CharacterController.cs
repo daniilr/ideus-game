b2db4bd44ff878d579df71988749795fac1387e8
Даниил Рыжков <daniil.re@gmail.com>
2015-04-25 07:05:22 +1000

    Forgot to save scene

--- IDEus/Assets/scripts/CharacterController.cs
+++ IDEus/Assets/scripts/CharacterController.cs
@@ -8,7 +8,8 @@ public class CharacterController : MonoBehaviour {
 	}
 	void Move(int blocks)
 	{
-		transform.Translate (new Vector3 (12, 0, 0));
+		GameObject blackbox = GameObject.Find ("BlackBlock");
+		transform.Translate (new Vector3 (blackbox.GetComponent<Collider2D>().bounds.size.x*blocks, 0, 0));
 	}
 	void Jump(int direction)
 	{	
@@ -27,6 +28,10 @@ public class CharacterController : MonoBehaviour {
 			Jump (-1);
 		if (Input.GetKeyDown (KeyCode.UpArrow))
 			Jump (1);
+		if (Input.GetKeyDown (KeyCode.RightArrow))
+			Move (1);
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+			Move (-1);
 		Rigidbody2D rb = GetComponent<Rigidbody2D>();
 		rb.AddForce (new Vector2(1f,0));
 	}
