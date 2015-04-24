78f126f69a9e46328c1cb8d542e4db0dd7dc28bf
Даниил Рыжков <daniil.re@gmail.com>
2015-04-25 06:38:26 +1000

    Character Jump

--- IDEus/Assets/scripts/CharacterController.cs
+++ IDEus/Assets/scripts/CharacterController.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 	
 	}
-	void MoveForward(int blocks)
+	void Move(int blocks)
 	{
-		transform.Translate (new Vector3 (1.7f, 0, 0));
+		transform.Translate (new Vector3 (12, 0, 0));
+	}
+	void Jump(int direction)
+	{	
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if (rb.velocity.magnitude < 0.01)
+			rb.AddForce (new Vector2 (direction*3f, 3f), ForceMode2D.Impulse);
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-			MoveForward (1);
+		if (Input.GetKeyDown (KeyCode.Space))
+			Jump (-1);
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			Jump (1);
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		rb.AddForce (new Vector2(1f,0));
+
 	}
 }
