f13d8b115eeb07227703660589b78361f4319bd6
Celikin <celikin@vl.ru>
2015-04-25 12:51:08 +1000

    added callbacks

--- IDEus/Assets/scripts/CharacterController.cs
+++ IDEus/Assets/scripts/CharacterController.cs
@@ -4,41 +4,47 @@ using System.Collections;
 public class CharacterController : MonoBehaviour {
 	const int FRONT = 1;
 	const int BACK = -1;
-	//object[][] action_queue = new string[] {new string[]{"Jump", FRONT}, new string[]{"Jump", BACK}};
 	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 	}
-	void Move(int blocks)
+
+	void Move(int direction)
 	{
 		GameObject blackbox = GameObject.Find ("BlackBlock");
-		transform.Translate (new Vector3 (blackbox.GetComponent<Collider2D>().bounds.size.x*blocks, 0, 0));
+		transform.Translate (new Vector3 (blackbox.GetComponent<Collider2D>().bounds.size.x*direction, 0, 0));
+		Callback(System.Reflection.MethodBase.GetCurrentMethod().Name);
 	}
+
 	void Jump(int direction)
 	{	
 		if (rb.velocity.magnitude < 0.01)
 			rb.AddForce (new Vector2 (direction*3f, 3f), ForceMode2D.Impulse);
+		Callback(System.Reflection.MethodBase.GetCurrentMethod().Name);
 	}
 
 	void Restart(){
 		Application.LoadLevel(Application.loadedLevel);
+		Callback(System.Reflection.MethodBase.GetCurrentMethod().Name);
+	}
+
+	void Callback(string eventName){
+		Application.ExternalCall( "callbackEvent", eventName);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space))
-			Jump (-1);
+			Jump (BACK);
 		if (Input.GetKeyDown (KeyCode.UpArrow))
-			Jump (1);
+			Jump (FRONT);
 		if (Input.GetKeyDown (KeyCode.RightArrow))
-			Move (1);
+			Move (FRONT);
 		if (Input.GetKeyDown (KeyCode.LeftArrow))
-			Move (-1);
-//		if (action_queue.Length > 0 && rb.velocity.magnitude < 0.01) {
-//
-//		}
+			Move (BACK);
+
 
 	}
 }
